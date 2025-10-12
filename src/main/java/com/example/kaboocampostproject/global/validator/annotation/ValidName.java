d58d07a43ec97012e72e16fb4d1891c2412883ae
seonghooncho <imalex124@gmali.com>
2025-10-12 23:43:37 +0900

    feat : 유효성 검사 어노테이션 추가

---
+++ src/main/java/com/example/kaboocampostproject/global/validator/annotation/ValidName.java
@@ -0,0 +1,18 @@
+package com.example.kaboocampostproject.global.validator.annotation;
+
+
+import com.example.kaboocampostproject.global.validator.NameValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = NameValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidName {
+    String message() default "이름은 2자 이상 20자 이하만 가능합니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/example/kaboocampostproject/global/validator/annotation/ValidPassword.java
@@ -0,0 +1,17 @@
+package com.example.kaboocampostproject.global.validator.annotation;
+
+import com.example.kaboocampostproject.global.validator.PasswordValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+    String message() default "비밀번호는 8자 이상이며, 영문자와 숫자, 특수문자를 포함해야 합니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
