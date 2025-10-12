7c5284b33aac8302eedf69374aa295aaec2963fe
seonghooncho <imalex124@gmali.com>
2025-10-12 23:41:48 +0900

    feat : member api 기능 개발
    1. member crud 기능개발
    2. memberProfile 정보(name, imageUrl) 은 redis 파이프라이닝을 이용해서 한번에 배치조회

---
+++ src/main/java/com/example/kaboocampostproject/domain/member/dto/request/UpdateMemberReqDTO.java
@@ -0,0 +1,18 @@
+package com.example.kaboocampostproject.domain.member.dto.request;
+
+import com.example.kaboocampostproject.global.validator.annotation.ValidName;
+import com.example.kaboocampostproject.global.validator.annotation.ValidPassword;
+
+public class UpdateMemberReqDTO {
+    public record MemberPassword (
+            @ValidPassword String oldPassword,
+            @ValidPassword String newPassword
+    )
+    { }
+    public record MemberName (
+            @ValidName String name
+    ){}
+    public record MemberProfileImage (
+            String imageUrl
+    ){}
+}
