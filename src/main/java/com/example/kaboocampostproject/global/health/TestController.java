79816182ede90302f5ac8e29c53db0372f07d5e8
seonghooncho <imalex124@gmali.com>
2025-12-06 16:25:41 +0900

    debug : redis 연결 확인
    - 로컬 환경에서 email 발송이 실패하는 오류 발생
    - 이메일 발송이전 elastic cache와의 문제로 보고, 확인할 수 있는 endpoint 추가

---
+++ src/main/java/com/example/kaboocampostproject/global/health/TestController.java
@@ -0,0 +1,23 @@
+package com.example.kaboocampostproject.global.health;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private StringRedisTemplate redis;
+
+    @GetMapping("/test-redis")
+    public String test() {
+        System.out.println("connect 성공");
+        redis.opsForValue().set("hello", "world");
+        System.out.println("set 성공");
+        redis.opsForValue().get("hello");
+        System.out.println("get 성공");
+        return redis.opsForValue().get("hello");
+    }
+}
